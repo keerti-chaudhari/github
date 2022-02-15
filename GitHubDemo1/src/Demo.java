dd34262554b3238ce6c2479ed1d12e6b05818b31
Kirti <Kirti@DESKTOP-6HVRKEU>
2022-02-15 12:17:37 +0530

    comm

---
+++ GitHubDemo1/src/Demo.java
@@ -0,0 +1,39 @@
+
+
+public class Demo {
+
+	
+	public void m1()
+	{
+		System.out.println("Hello m1");
+	}
+	
+	public void m2()
+	{
+		
+		System.out.println("Hello m2");
+	}
+	
+		public void m3()
+	{
+		
+		System.out.println("Hello m3");
+	}
+	
+		public void m4()
+	{
+		
+		System.out.println("Hello m4");
+	}
+	
+	public static void main(String[] args) {
+
+		
+		System.out.println("Hello Main of GIT");
+		
+		Demo d=new Demo();
+		d.m1();
+		d.m2();
+	}
+
+}
